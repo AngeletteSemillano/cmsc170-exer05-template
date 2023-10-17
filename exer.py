f946a295c57dac3719309da46c7a86c456575937
Princess Angelette Semillano <125256432+AngeletteSemillano@users.noreply.github.com>
2023-10-17 05:15:55 +0000

    exer

--- exer.py
+++ exer.py
@@ -3,6 +3,7 @@ import re
 
 #BAG OF WORDS COMPONENTS
 filecontent = []
+classifywords = []
 hambagofwords = {}
 spambagofwords = {}
 classify = {}
@@ -12,17 +13,20 @@ hamtotalwords = 0
 hamdictionarycount = 0
 spamtotalwords = 0
 spamdictionarycount = 0
+count = 1
 
 
 #--------------------- RETRIEVE WORDS -------------------------#
 
-def getAllWords(file,filecontent):
+def getAllWords(txtfile,filecontent):
      #OPEN THE INPUT FILE
-    file = open(file, "r")
-    text = file.readlines()
+    # file = open(txtfile, "r")
+    with open(txtfile, encoding="utf8", errors='ignore') as file:
+        text = file.readlines()
 
     #GENERATE A LIST OF STRING FROM THE INPUT FILE
     for i in text:
+
         filecontent.extend(i.split())
 
     file.close()
@@ -43,8 +47,7 @@ def getAllWords(file,filecontent):
         f.write(i +"\n")
         
     f.close()
-
-
+        
 # --------------------- BOG FUNCTION --------------------- #
 def bog(filecontent,bagofwords,totalwords,dictionarycount):
     
@@ -61,54 +64,113 @@ def bog(filecontent,bagofwords,totalwords,dictionarycount):
 
     for i in bagofwords.keys():
         dictionarycount = dictionarycount  + 1
+
+    return dictionarycount
         
     # for i in bagofwords.keys():
     #     print(i)   
         
         
-    #WRITE RESULTS IN AN OUTPUT FILE
-    f = open("output.out", "w")
+    # #WRITE RESULTS IN AN OUTPUT FILE
+    # f = open("output.out", "w")
 
-    f.writelines('Dictionary Size:  ' + repr(dictionarycount) + "\n")
-    f.writelines("Total Number of Words: " +repr(totalwords)+"\n")
+    # f.writelines('Dictionary Size:  ' + repr(dictionarycount) + "\n")
+    # f.writelines("Total Number of Words: " +repr(totalwords)+"\n")
 
-    for i in bagofwords.keys():
-        i = (str(i)).replace('"', '')
-        f.write(i + " " + repr(bagofwords[i]).strip('')+"\n")
+    # for i in bagofwords.keys():
+    #     i = (str(i)).replace('"', '')
+    #     f.write(i + " " + repr(bagofwords[i]).strip('')+"\n")
         
-    f.close()
+    # f.close()
 
 
-# -------------------- SPAM OR HAM ------------------- #
-def classifyIfSpam(word):
+# -------------------- GET SPAM/HAM PROBABILITY ------------------- #
+def classifySpamHam(text,bagofwords,dictionarycount):
+
+    #list of the probability of each word found in the spam data
+    probabilityList = []
+
+    # stores the spam probability of the file 
+    prob = 1    
 
     #search for the word in the dictionary
-    for word in 
+    for word in text:
+        if word in bagofwords.keys():
+            ans = bagofwords[word]/dictionarycount
+            probabilityList.append(ans)
+    
+    for i in probabilityList:
+        prob = prob * i
+
+    return prob
 
 
 
 
-
-
-classifyDataPath = "C:/Users/angel/Desktop/cmsc170-exer05-template-main/data/data01/classify"
-hamDataPath = "C:/Users/angel/Desktop/cmsc170-exer05-template-main/data/data01/ham"
-spamDataPath = "C:/Users/angel/Desktop/cmsc170-exer05-template-main/data/data01/spam"
+classifyDataPath = "/workspaces/cmsc170-exer05-template/data/data01/classify"
+hamDataPath = "/workspaces/cmsc170-exer05-template/data/data01/ham"
+spamDataPath = "/workspaces/cmsc170-exer05-template/data/data01/spam"
 
 classifyData = os.listdir(classifyDataPath)
 hamData = os.listdir(hamDataPath)
 spamData = os.listdir(spamDataPath)
 
-# for file in sorted(hamData):
-#     pathA = "C:/Users/angel/Desktop/cmsc170-exer05-template-main/data/data01/ham/"+file
-#     getAllWords(pathA,filecontent)
+for file in sorted(hamData):
+    pathA = "/workspaces/cmsc170-exer05-template/data/data01/ham/"+file
+    getAllWords(pathA,filecontent)
 
-# bog(filecontent,hambagofwords,hamtotalwords,hamdictionarycount)
+hamdictionarycount = bog(filecontent,hambagofwords,hamtotalwords,hamdictionarycount)
     
 filecontent.clear()
 
 for file in sorted(spamData):
-    pathB = "C:/Users/angel/Desktop/cmsc170-exer05-template-main/data/data01/spam/"+file
+    pathB = "/workspaces/cmsc170-exer05-template/data/data01/spam/"+file
     getAllWords(pathB,filecontent)
 
-bog(filecontent,spambagofwords,spamtotalwords,spamdictionarycount)
+spamdictionarycount = bog(filecontent,spambagofwords,spamtotalwords,spamdictionarycount)
 
+filecontent.clear()
+
+f = open("output.out", "w")
+
+
+for file in sorted(classifyData):
+    pathC= "/workspaces/cmsc170-exer05-template/data/data01/classify/"+file
+    getAllWords(pathC,filecontent)
+    #remove duplicates in the classifydata
+    classifywords = sorted(list(dict.fromkeys(filecontent)))
+    spamProbability = classifySpamHam(classifywords,spambagofwords,spamdictionarycount)
+    hamProbability = classifySpamHam(classifywords,hambagofwords,hamdictionarycount)
+    f.writelines(str(count) + " SPAM: " + str(spamProbability) + " HAM: " + str(hamProbability) + "\n")
+    
+    # update counter
+    count = count + 1
+
+    # reset the list that stores the words
+    classifywords.clear()
+    filecontent.clear()
+
+
+    
+
+
+    # if spamProbability > hamProbability:
+    #     f.write(count +" SPAM "+ spamProbability+"\n")
+    #     count = count + 1
+
+    # elif hamProbability < spamProbability:
+    #     f.write(count +" HAM "+ spamProbability+"\n")
+    #     count = count + 1
+
+
+f.close()
+
+
+
+
+
+
+     
+    #WRITE RESULTS IN AN OUTPUT FILE
+
+        
